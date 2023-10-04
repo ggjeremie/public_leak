9d6b20b05b0aebbc1055032b910e379ea32f1016
Jérémie Zitoun <jeremie.zitoun@gitguardian.com>
2023-10-04 09:17:46 +0200

    slack leaks

--- code.py
+++ code.py
@@ -4,3 +4,5 @@ slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99va3"
 slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99va4"
 slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99va5"
 
+slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99va6"
+
